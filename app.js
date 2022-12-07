b62bc5314591451d33483124ba64e114f63f5f70
Adam Alexander <adamjalexander88@gmail.com>
2022-12-07 18:22:29 +0000

    update

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 const form = document.getElementById('form');
 const inputs = document.querySelectorAll('form input');
 const email = document.getElementById('email');
+let returnedInputs = [];
 
 // const form = document.getElementById('form');
 
 function checkInputs(e){
     e.preventDefault();
-    let returnedInputs = '';
-    let inputID = inputs.forEach(input => {
-        if(input.id !== ''){
-            
-            returnedInputs = input;
-        }
+    // get inputs that have ID
+    inputs.forEach(input => input.id !== '' ? returnedInputs.push(input.value) : console.log('nothing'));
 
-        console.log(returnedInputs);
-    })
-
-    setTimeout(() => {
-        console.log(inputID);
-    }, 2000); 
+    // if returned inputs has any empty strings fire error function  
+    console.log(returnedInputs);
 }
 
 form.addEventListener('submit', (e) => checkInputs(e));
+
+// Clear form input data
+window.addEventListener('DOMContentLoaded', () => {
+    returnedInputs = [];
+    inputs.forEach(input => input.id !== '' ? input.value = '' : '');
+})
